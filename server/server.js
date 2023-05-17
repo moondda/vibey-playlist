e42d330fd3cbf18fde3eeccb88a4dc90f513a77f
millinova98 <kcong1@naver.com>
2023-05-17 17:19:53 +0900

    feat:signup_login_complete

--- server/server.js
+++ server/server.js
@@ -1,27 +1,29 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const cors=require('cors');
+const cors = require('cors');
 const bodyParser = require('body-parser');
-const mongoose=require('mongoose');
-const signupRouter=require('./routes/signupRouter');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const authRouter = require('./routes/signupRouter');
+const validRouter = require('./routes/validRouter');
+const User = require('./models/User');
 
+require('dotenv').config({ path: 'variables.env' });
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-require('dotenv').config({path:'variables.env'});
-app.use(bodyParser.urlencoded({extended:true}));
-
-  mongoose.connect(process.env.MONGODB_URL, {
+mongoose.connect(process.env.MONGODB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-dbName:'vibey-playlist',
+  dbName: 'vibey-playlist',
 })
-
-.then(() => {
-  console.log("Connected to MongoDB");
-})
-.catch((err) => {
-  console.log(err);
-}); // allows to use local MongoDB 
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  }); // allows to use local MongoDB 
 
 app.listen(5000, function () {
   console.log('listening on 5000')
@@ -31,16 +33,18 @@ app.use(express.json());
 app.use(cors());
 
 // app.use('/user',user);
-app.post('/auth/sign-up',signupRouter );
+app.use('/auth', authRouter);
+app.use('/valid', validRouter);
 
-
-app.use(express.static(path.join(__dirname,'./client/build')));
-app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname,'./client/build/index.html'));
+app.use(express.static(path.join(__dirname, './client/build')));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
 })
 // 수정완료
 
-//항상 가장 하단에 위치해야하는 코드
-app.get('*',function(req,res){
-  res.sendFile(path.join(__dirname,'./client/build/index.html'));
+//항상 가장 하단에 위치해야 하는 코드
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
 })
+
+module.exports = app; // app을 export합니다.
