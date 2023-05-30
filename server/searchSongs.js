c0406aba0915c4feece58b393bcb498ede476349
moondda <moondda@moondda-MacBookAir.local>
2023-05-30 18:56:11 +0900

    itunes-api

--- server/searchSongs.js
+++ server/searchSongs.js
@@ -2,6 +2,38 @@ const axios = require('axios');
 const { json } = require('body-parser');
 let parseString = require('xml2js').parseString;
 
+//https://www.maniadb.com/api/search/gee/?sr=song&display=10&key=example&v=0.5
+
+
+async function searchSongbyTitle(title) {
+  try {
+    const response = await axios.get('https://www.maniadb.com/api/search/' + title, {
+      params: {
+        sr: 'song',
+        display: 10,
+        key: 'example',
+        v: '0.5'
+      },
+    });
+
+    const xmlData = response.data;
+
+    const jsonData = await new Promise((resolve, reject) => {
+      parseString(xmlData, (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+    console.log(JSON.stringify(jsonData));
+  }
+  catch {
+    console.error("Error",error);
+  }
+}
+
 async function searchSong(artist) {
   try {
     const response = await axios.get('https://www.maniadb.com/api/search/' + artist, {
@@ -40,7 +72,7 @@ async function searchSong(artist) {
   }
 }
 
-searchSong('소녀시대')
+searchSongbyTitle('gee')
   .then(() => {
     console.log('Success!');
   })
