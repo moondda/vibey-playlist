563c74e7178d1213c97712a47adcde35181b2fe9
chamny20 <chamny20@naver.com>
2023-06-03 16:19:47 +0900

    feat:connect recommend api in fe

--- server/client/src/pages/recommend/TodayMusicPage.js
+++ server/client/src/pages/recommend/TodayMusicPage.js
@@ -3,11 +3,13 @@ import TodayMusic from "./TodayMusic";
 import NotiBox from "../../components/notification/NotiBox";
 import { AddModal } from "../../components/notification/AddModal";
 import FootBar from "../../components/footer/FootBar";
+import TodayTemp from "./TodayMusic";
 
 export default function TodayMusicPage() {
   return (
     <div className="App">
       <TodayMusic />
+      {/* <TodayTemp /> */}
       {/* <NotiBox noti="You have deleted successfully!"></NotiBox> */}
       <AddModal />
       <FootBar />
