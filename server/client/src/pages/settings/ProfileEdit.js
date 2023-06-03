9ff017ce3cd2bb4a40c34106d2951019fbe95ebe
moondda <moondda@moondda-MacBookAir.local>
2023-06-04 04:10:33 +0900

    feat:profile-edit connect

--- server/client/src/pages/settings/ProfileEdit.js
+++ server/client/src/pages/settings/ProfileEdit.js
@@ -1,32 +1,83 @@
-import React, { useState } from "react";
-import Profile from "../mypage/Profile";
+import React, { useState ,useEffect} from "react";
 import styled from "styled-components";
 import editImg from "../../assets/profileImg.jpg";
+import axios from "axios";
+import { useNavigate } from "react-router";
 
-const ProfileEdit = (props) => {
-  const [descript, setDescript] = useState("");
+const ProfileEdit = () => {
+  const [bio, setBio] = useState("");
   const [nick, setNick] = useState("");
 
-  // const handleNickname = (e) => {
-  //   axios
-  //     .post("http://localhost:5000/setting/nickname", {
-  //       nickname: nick,
-  //     })
-  //     .then((res) => {
-  //       console.log("res.data :", res.data);
-  //       if (res.data.result == true) {
-  //         alert(res.data.message);
-  //         console.log(res.data.message);
-  //         // setConfirmPwdValid(true);
-  //       } else {
-  //         console.log(res.data.message);
-  //         // setConfirmPwdValid(false);
-  //       }
-  //     })
-  //     .catch((err) => {
-  //       console.log("확인 실패:", err);
-  //     });
-  // };
+  const [biotrue,setBioTrue]=useState(false);
+  const [nicktrue,setNickTrue] =useState(false);
+
+  const navigate = useNavigate();
+
+  const viewMyInfo = () => {
+    axios
+      .get("http://localhost:5000/user/info", {
+        headers: {
+          Authorization: `${sessionStorage.getItem("user_token")}`,
+        },
+      })
+      .then((res) => {
+        console.log("res.data:", res.data);
+        setBio(res.data.bio);
+        setNick(res.data.nickname);
+        console.log("myBio:", bio);
+        console.log("myNick:", nick);
+      })
+      .catch((err) => {
+        console.log("Error", err);
+      });
+  };
+
+  const handleNickBio = async () => {
+    try {
+      await axios.post(
+        "http://localhost:5000/setting/nickname",
+        { nickname: nick },
+        {
+          headers: {
+            Authorization: `${sessionStorage.getItem("user_token")}`,
+          },
+        }
+      );
+      console.log("myNick:", nick);
+      setNickTrue(true);
+  
+      await axios.post(
+        "http://localhost:5000/setting/bio",
+        { bio: bio },
+        {
+          headers: {
+            Authorization: `${sessionStorage.getItem("user_token")}`,
+          },
+        }
+      );
+      console.log("myBio:", bio);
+      setBioTrue(true);
+  
+      handleNavigation();
+    } catch (err) {
+      console.log("Error", err);
+    }
+  };
+  
+  const handleNavigation = () => {
+    if (biotrue && nicktrue) {
+      navigate("/profile");
+    }
+  };
+
+  useEffect(() => {
+    console.log("useEffect실행");;
+    viewMyInfo();
+  }, []);
+
+  useEffect(() => {
+    handleNavigation();
+  }, [biotrue, nicktrue]);
 
   return (
     <DescriptContainer>
@@ -54,12 +105,12 @@ const ProfileEdit = (props) => {
           </ProfileImg>
         </div>
         <div>
-          <InputFieldWrapper>
+        <InputFieldWrapper>
             <EditText style={{ color: "white", textAlign: "left" }}>
               닉네임
             </EditText>
             <div style={{ display: "flex" }}>
-              <InputField placeholder="닉네임"></InputField>
+              <InputField placeholder="닉네임" value={nick} onChange={(e) => setNick(e.target.value)} />
             </div>
           </InputFieldWrapper>
           <InputFieldWrapper>
@@ -67,12 +118,12 @@ const ProfileEdit = (props) => {
               한 줄 소개
             </EditText>
             <div style={{ display: "flex" }}>
-              <InputField placeholder="한줄소개"></InputField>
+              <InputField placeholder="한줄소개" value={bio} onChange={(e) => setBio(e.target.value)} />
             </div>
           </InputFieldWrapper>
         </div>
       </div>
-      <ButtonBox>저장</ButtonBox>
+      <ButtonBox onClick={handleNickBio}>저장</ButtonBox>
     </DescriptContainer>
   );
 };
