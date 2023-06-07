04f89554f7388967085a1b00c28e518db7e49717
moondda <moondda@moondda-MacBookAir.local>
2023-06-08 03:11:00 +0900

    feat:deleteSong connect

--- server/client/src/pages/mypage/Profile.js
+++ server/client/src/pages/mypage/Profile.js
@@ -4,8 +4,6 @@ import { Link, useLocation } from "react-router-dom";
 import styled from "styled-components";
 
 export default function Profile() {
-  const location = useLocation();
-  const searchParams = new URLSearchParams(location.search);
   const [myMusic, setMyMusic] = useState(null);
 
   const [mp4, setMp4] = useState("");
@@ -14,7 +12,8 @@ export default function Profile() {
   const [selectedAudio, setSelectedAudio] = useState(null);
 
   const [trackId, setTrackId] = useState("");
-  const [objId, setObjId] = useState("");
+
+  const location = useLocation();
   const nickname = location.pathname.split("/")[2];
 
   const handleImageClick = (audioUrl) => {
@@ -24,42 +23,43 @@ export default function Profile() {
   };
 
   const viewMyFeed = async (nickname) => {
-    if (nickname) {
+    if(nickname){
       console.log(nickname);
       axios
-        .get(`http://localhost:5000/song/other-post/${nickname}`, {})
-        .then((res) => {
-          console.log("res.data입니다:", res.data);
-          setMyMusic(res.data);
-          setMp4(res.data.mp4);
-          setTrackId(res.data.trackId);
-          setObjId(res.data._id);
-          console.log("myMusic:", myMusic);
-          console.log("mp4:", mp4);
-        })
-        .catch((err) => {
-          console.log("Error", err);
-        });
-    } else {
-      axios
-        .get("http://localhost:5000/song/mypost", {
-          headers: {
-            Authorization: `${sessionStorage.getItem("user_token")}`,
-          },
-        })
-        .then((res) => {
-          console.log("res.data입니다:", res.data);
-          setMyMusic(res.data);
-          setMp4(res.data.mp4);
-          setObjId(res.data._id);
-          setTrackId(res.data.trackId);
-          console.log("myMusic:", myMusic);
-          console.log("mp4:", mp4);
-        })
-        .catch((err) => {
-          console.log("Error", err);
-        });
+      .get(`http://localhost:5000/song/other-post/${nickname}`, {
+
+      })
+      .then((res) => {
+        console.log("res.data입니다:", res.data);
+        setMyMusic(res.data);
+        setMp4(res.data.mp4);
+        setTrackId(res.data.trackId);
+        console.log("myMusic:", myMusic);
+        console.log("mp4:", mp4);
+      })
+      .catch((err) => {
+        console.log("Error", err);
+      });
+
     }
+    else{
+    axios
+      .get("http://localhost:5000/song/mypost", {
+        headers: {
+          Authorization: `${sessionStorage.getItem("user_token")}`,
+        },
+      })
+      .then((res) => {
+        console.log("res.data입니다:", res.data);
+        setMyMusic(res.data);
+        setMp4(res.data.mp4);
+        setTrackId(res.data.trackId);
+        console.log("myMusic:", myMusic);
+        console.log("mp4:", mp4);
+      })
+      .catch((err) => {
+        console.log("Error", err);
+      });}
   };
 
   useEffect(() => {
@@ -68,14 +68,12 @@ export default function Profile() {
     viewMyFeed(nickname);
   }, [nickname]);
 
-  console.log("objId:", objId);
-
   return (
     <div
       style={{
         width: "100%",
         position: "absolute",
-        top: "420px",
+        top: "370px",
       }}
     >
       <GridBox>
@@ -83,7 +81,7 @@ export default function Profile() {
           myMusic.map((musicData, index) => {
             return (
               <Item key={index}>
-                <Link to={`/feed-result?${musicData._id}`}>
+                <Link to={`/search-result?trackId=${musicData.trackId}`}>
                   <img
                     src={musicData.albumCover}
                     style={{ width: "100%" }}
@@ -107,7 +105,7 @@ const GridBox = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   /* box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); */
-  max-height: 355px;
+  max-height: 400px;
   overflow-y: auto;
   /* justify-content: center; */
   /* align-items: center; */
@@ -124,7 +122,7 @@ const Item = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  /* height: 96px;
-  width: 100px; */
+  /* height: 100%; */
+  /* width: 100%; */
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 `;
